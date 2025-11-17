3045509836908abe5d615923c0a80da0ce799664
dipzza <dipzza@disroot.org>
2025-11-17 15:47:07 +0100

    Update free iceServers

--- config/peerServer.js
+++ config/peerServer.js
@@ -3,9 +3,10 @@ export const ROOM_B_ID = '472d306b-1dae-43fd-b619-9f4a023d3082';
 export const ROOM_C_ID = '1b89c202-39e3-4dba-8646-069a18a55831';
 export const ROOM_D_ID = '3f862454-86d7-4285-b545-e11fdbd33750';
 
+const response = 
+  await fetch("https://dipzza.metered.live/api/v1/turn/credentials?apiKey=de7751b1831959da36efac9771311a6bf31f");
+const iceServers = await response.json();
+
 export const serverConfig = {
-  iceServers: [
-      {urls: 'stun:freestun.net:3478' }, 
-      {urls: 'turn:freestun.net:3478', username: 'free', credential: 'free' }
-  ]
+  iceServers
 }
